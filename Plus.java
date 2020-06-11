51aee2968203a27f1849b9fc5a3029853fbc4b39
5710110484 <5710110484@psu.ac.th>
2020-06-12 00:44:46 +0700

    2

--- Plus.java
+++ Plus.java
@@ -2,7 +2,7 @@ public class Plus
 {
     public static void main(String args[])
     {
-        int a =4,b=5;
-        System.out.println("sum = "+(a+b));
+        int a =4,b=5,c=3;
+        System.out.println("sum = "+(a+b+c));
     }
 }
