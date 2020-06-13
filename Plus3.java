5248e06146675f54ecaac7f0b3e593b44fde0155
Satinee <inspiminato@gmail.com>
2020-06-13 18:35:26 +0700

    Create Plus3.java

---
+++ Plus3.java
@@ -0,0 +1,14 @@
+public class Plus3
+{
+    public static void main(String args[])
+    {
+        int a =5,b=2;
+        sum(a,b);
+        
+    }
+
+    public static void sum(int a, int b)
+    {
+        System.out.println("Sum = "+(a+b));
+    }
+}
