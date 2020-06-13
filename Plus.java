5bfb9b7ad5fac9f725417d9a62f1644bf567fbab
5710110484 <5710110484@psu.ac.th>
2020-06-13 18:25:54 +0700

    3

--- Plus.java
+++ Plus.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 public class Plus
 {
     public static void main(String args[])
@@ -6,13 +6,11 @@ public class Plus
         int a =4,b=5,c=3;
         System.out.println("sum = "+(a+b+c));
     }
-=======
+
 public class Plus
 {
     public static void main(String args[])
     {
         int a =4,b=5,c=3;
         System.out.println("sum = "+(a+b+c));
-    }
->>>>>>> 92be82d09792e64768635e5b7a1a434c94b1a8b7
 }
---
+++ Plus2.java
@@ -0,0 +1,13 @@
+public class Plus2
+{
+    public static void main(String args[])
+    {
+        int a =5,b=2;
+        System.out.println("Sum = "+sum(a,b));
+    }
+
+    public static int sum(int a, int b)
+    {
+        return a+b;
+    }
+}
