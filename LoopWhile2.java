7ab3ed5869cc6a7c62b7ee4c3dff5ff5f1ac602c
Satinee <inspiminato@gmail.com>
2020-06-17 14:06:48 +0700

    17(2)

---
+++ LoopWhile2.java
@@ -0,0 +1,12 @@
+public class LoopWhile2
+{
+    public static void main(String args[])
+    {
+        int i=0;
+        while(i<30)
+        {
+            System.out.println(i+1);
+            i++;
+        }
+    }
+}
