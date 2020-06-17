468a707206422090ea8650d5416b168728a7d979
Satinee <inspiminato@gmail.com>
2020-06-17 14:16:38 +0700

    17(3)

---
+++ LoopWhile3.java
@@ -0,0 +1,12 @@
+public class LoopWhile3
+{
+    public static void main(String args[])
+    {
+        int i = 2;
+        while(i<=10)
+        {
+            System.out.print(i+"\t");
+            i+=3;
+        }
+    }
+}
